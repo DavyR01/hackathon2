e7dae6418d62295de1a44ffec6033d968d7eb37b
Davy ROBERT <davyrobert90@gmail.com>
2023-01-11 11:42:45 +0100

    first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,11 @@
-import Home from "./pages/Home";
-
 import "./App.css";
+import Test from "./components/Test";
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <p>coucou</p>
+      <p className="bg-green bg-slate-600 text-yellow-700">Hello !!</p>
+      <Test />
     </div>
   );
 }
