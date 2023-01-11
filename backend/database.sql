9858e63e6ce137c9c2d3bf8a774e93d08a7855d5
arnaudchampetier <arnaud.champetier9@gmail.com>
2023-01-11 16:41:06 +0100

    Ajout BDD et Routes première mouture fonctionnelle

--- backend/database.sql
+++ backend/database.sql
@@ -1,6 +1,6 @@
-DROP TABLE IF EXISTS vehicule;
+DROP TABLE IF EXISTS vehicle;
 
-CREATE TABLE vehicule (
+CREATE TABLE vehicle (
   id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   brand VARCHAR(80) NOT NULL,
   model VARCHAR(80) NOT NULL,
@@ -12,7 +12,7 @@ CREATE TABLE vehicule (
 -- ENGINE = InnoDB;
 );
 
-INSERT INTO vehicule (brand, model, mileage, technicalData, localisation, isAvailable, inRepair) VALUES ('Peugeot', '206', '150000 km', 'Diesel', 'Lyon', '1', '0');
+INSERT INTO vehicle (brand, model, mileage, technicalData, localisation, isAvailable, inRepair) VALUES ('Peugeot', '206', '150000 km', 'Diesel', 'Lyon', '1', '0');
 
 DROP TABLE IF EXISTS user;
 
@@ -27,11 +27,11 @@ CREATE TABLE user (
   isAdmin TINYINT NULL,
   avatar VARCHAR(255) NULL,
   date_creation DATETIME NOT NULL DEFAULT NOW(),
-  vehicule_id INT,
+  vehicle_id INT,
   -- CONSTRAINT `fk_user_vehicule`
-  FOREIGN KEY (vehicule_id) REFERENCES vehicule (id)
+  FOREIGN KEY (vehicle_id) REFERENCES vehicle (id)
 );
 -- ENGINE = InnoDB;
 
-INSERT INTO user (firstname, lastname, email, city, phone, hashedPassword, isAdmin, avatar, date_creation, vehicule_id) VALUES ('Davy', 'Mccree', 'davy.mccree@gmail.com', 'Oslo', '0600000000', '12345', '1', 'monavatar.png', '2022-10-13 12:12:23', '1');
+INSERT INTO user (firstname, lastname, email, city, phone, hashedPassword, isAdmin, avatar, date_creation, vehicle_id) VALUES ('Davy', 'Mccree', 'davy.mccree@gmail.com', 'Oslo', '0600000000', '12345', '1', 'monavatar.png', '2022-10-13 12:12:23', '1');
 
