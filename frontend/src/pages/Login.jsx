47686f5566c30f1c4e855784c9bcad6f0352d630
quentinferraripro <ferrari.quentin@gmail.com>
2023-01-11 17:59:06 +0100

    css login fonctionnel

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,39 @@
 import React from "react";
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="h-screen flex items-center justify-center relative">
+      <div className="">
+        <h1 className="text-center absolute top-10 left-32">Connectez-vous</h1>
+        <form className="m-auto mt-5">
+          <div className="mb-3">
+            <p>Adresse e-mail</p>
+            <input
+              type="email"
+              placeholder="email"
+              className="form-control border-solid border-black border-2 rounded-lg"
+              id="email"
+            />
+          </div>
+          <div className="mb-3">
+            <p>Mot de passe</p>
+            <input
+              type="password"
+              placeholder="mot de passe"
+              className="form-control border-solid border-black border-2 rounded-lg"
+              id="password"
+            />
+          </div>
+          <button
+            type="submit"
+            className="hover:bg-green-600 hover:text-white bg-green-500 text-white border-solid boder-black border-2 rounded-lg"
+          >
+            Connexion
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
