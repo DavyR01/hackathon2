58f4cb3a5e91373803d11169a004cdcb47c10e7f
Davy ROBERT <davyrobert90@gmail.com>
2023-01-12 18:32:36 +0100

    up

--- frontend/src/pages/Reserved.jsx
+++ frontend/src/pages/Reserved.jsx
@@ -41,6 +41,14 @@ function Reserved() {
           </p>
         </div>
       </div>
+      <div className="flex justify-end">
+        <button
+          type="button"
+          className="mr-10 mt-4 bg-black text-white buttonExplorer text-white text-center mb-24 font-medium rounded-lg text-lg px-4 py-2 border hover:scale-105 duration-300 border-black opacity-50 hover:opacity-100 border-2"
+        >
+          RESERVER
+        </button>
+      </div>
       <Navbar />
     </div>
   );
