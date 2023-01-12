ef2798afdb8c9872629d53ccff39f18417317382
SixcoH <samy.dumas69100@gmail.com>
2023-01-12 18:38:30 +0100

    call api on front + css

--- frontend/src/pages/VehiculeList.jsx
+++ frontend/src/pages/VehiculeList.jsx
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
+import "../styles/vehiculeList.css";
+import audi from "../assets/audi.png";
 
 function VehiculeList() {
+  const [cars, setCars] = useState([]);
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  useEffect(() => {
+    fetch("http://localhost:5000/vehicles")
+      .then((res) => res.json())
+      .then((vehiclesList) => setCars(vehiclesList));
+  }, []);
+
   return (
-    <div>
+    <div className="container flex justify-center flex-col items-center gap-8">
+      {cars.map((car) => (
+        <div className="containertest bg-white">
+          <div className="" key={car.id}>
+            <h1 className="containerh1"> {car.brand} </h1>
+            <div className=" ">
+              <h3 className="containerh3"> {car.model}</h3>
+            </div>
+            <div className="container-techi">
+              <h3 className="container-h3">{car.technicalData}</h3>
+            </div>
+            <div>
+              <img className="container-audi" src={audi} alt="audi" />
+            </div>
+          </div>
+          <button
+            type="button"
+            className="container-btn border border-indigo-500 bg-[#497174] text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-indigo-600 focus:outline-none focus:shadow-outline"
+          >
+            Primary
+          </button>
+        </div>
+      ))}
       <Navbar />
     </div>
   );
