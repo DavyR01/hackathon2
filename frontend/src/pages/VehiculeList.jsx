9f424a9df0c43f534f8ecd4d7ec3eaa55faa2649
SixcoH <samy.dumas69100@gmail.com>
2023-01-12 10:39:38 +0100

    Navbar finish and new colors boody

--- frontend/src/pages/VehiculeList.jsx
+++ frontend/src/pages/VehiculeList.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import VehiculesList from "../components/VehiculesList";
 import Navbar from "../components/Navbar";
 
 function VehiculeList() {
   return (
     <div>
-      <VehiculesList />
       <Navbar />
     </div>
   );
